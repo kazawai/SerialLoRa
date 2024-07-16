7e1d7b8e4b1bd3c89070eb06c71253267a9b6931
kazawai <bazinmaxime0@gmail.com>
2024-07-16 13:28:56 +0200

    Added socket options and cleaned stuff

--- serial_controller.py
+++ serial_controller.py
@@ -4,8 +4,12 @@ from serial.tools import list_ports
 import sys
 import signal
 import time
-from colors import bcolors
 import socket
+from colors import bcolors
+
+# Import the dictionary that maps the serial number to the device ID
+# Replace with your own dictionary
+from serials_list import SERIAL_TO_ID
 
 D_TIME_BETWEEN_PORTS = 1
 
@@ -14,57 +18,21 @@ D_TIME_BETWEEN_PORTS = 1
 DEVICE_LIST = list(list_ports.grep("ACM"))
 SERIAL_PORT_LIST = [(port.device, port.serial_number) for port in DEVICE_LIST]
 
-SERIAL_TO_ID = {
-    "7DF194D65030574B412E3120FF191C07": "1",
-    "360931A35030574B412E3120FF15190F": "2",
-    "0ED7D81E5030574B412E3120FF160839": "3",
-    "7B2DD5C55030574B412E3120FF160A14": "4",
-    "FFC5ED415030574B412E3120FF130729": "5",
-    "EF5CE8325030574B412E3120FF13072D": "6",
-    "3B66C4E25030574B412E3120FF130616": "7",
-    "54E4203B5030574B412E3120FF13071F": "8",
-    "1A925BDC5030574B412E3120FF15062A": "9",
-    "B12EED6F5030574B412E3120FF16091E": "10",
-    "BB7AFFBE5030574B412E3120FF160810": "11",
-    "D46A0F5B5030574B412E3120FF15172E": "12",
-    "02BC80115030574B412E3120FF160C15": "13",
-    "3B9F09355030574B412E3120FF160C27": "14",
-    "5210D2305030574B412E3120FF142A3A": "15",
-    "53E0564F5030574B412E3120FF13072C": "16",
-    "63174B875030574B412E3120FF15290A": "17",
-    "CC5630315030574B412E3120FF180A38": "18",
-    "3E321F2A5030574B412E3120FF140421": "19",
-    "DABE48365030574B412E3120FF130924": "20",
-    "0683FD0F5030574B412E3120FF152739": "21",
-    "4E1FD0D35030574B412E3120FF13093B": "22",
-    "3955ACBF5030574B412E3120FF191D38": "23",
-    "A8E7B2285030574B412E3120FF13072B": "24",
-    "15EA004F5030574B412E3120FF160C20": "25",
-    "48C909845030574B412E3120FF160B34": "26",
-    "DFA6BCB15030574B412E3120FF151711": "27",
-    "8C7DE18D5030574B412E3120FF180C15": "28",
-    "518680F75030574B412E3120FF152710": "29",
-    "8146287150593730372E3120FF031A30": "30",
-    "47BFC0915030574B412E3120FF14233D": "31",
-    "062B95965030574B412E3120FF191D39": "32",
-    "F003CE685030574B412E3120FF130A11": "33",
-    "71EFE65E5030574B412E3120FF13061A": "34",
-    "E019727B5030574B412E3120FF191B09": "35",
-    "31CA30465030574B412E3120FF180C0D": "36",
-}
-
 SERVER_PORT = 12345
 SERVER_IP = "localhost"
 
-try:
-    data_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    data_connection.connect((SERVER_IP, SERVER_PORT))
-except ConnectionRefusedError:
-    print(
-        f"{bcolors.FAIL}Connection to server {SERVER_IP}:{SERVER_PORT} refused, impossible to communicate with receiver. Exiting{bcolors.ENDC}",
-        file=sys.stderr,
-    )
-    sys.exit(1)
+data_connection = None
+
+if "-r" in sys.argv:
+    try:
+        data_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data_connection.connect((SERVER_IP, SERVER_PORT))
+    except ConnectionRefusedError:
+        print(
+            f"{bcolors.FAIL}Connection to server {SERVER_IP}:{SERVER_PORT} refused, impossible to communicate with receiver. Exiting{bcolors.ENDC}",
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
 
 class SerialController:
@@ -102,11 +70,24 @@ class SerialController:
         return self.ser.write(data)
 
     def read(self):
-        if self.ser.in_waiting == 0:
+        try:
+            if self.ser.in_waiting == 0:
+                return b""
+            if self.skip:
+                return b""
+            return self.ser.read()
+        except SerialException:
+            print(
+                f"{bcolors.FAIL}Error reading from port {self.port} : DEVICE {self.device_id}{bcolors.ENDC}",
+                file=sys.stderr,
+            )
+            self.skip = True
             return b""
-        if self.skip:
-            return b""
-        return self.ser.read()
+        except OSError:
+            print(
+                f"{bcolors.WARNING}I/O error on port {self.port} : DEVICE {self.device_id}{bcolors.ENDC}"
+            )
+            self.skip = True
 
     def readline(self):
         return self.ser.readline()
@@ -226,8 +207,9 @@ if __name__ == "__main__":
                 f"{bcolors.FAIL}Some devices are not in the SERIAL_TO_ID dictionary, exiting{bcolors.ENDC}",
                 file=sys.stderr,
             )
-            data_connection.send(b"close")
-            data_connection.close()
+            if data_connection:
+                data_connection.send(b"close")
+                data_connection.close()
             sys.exit(1)
 
         # Sort the SERIAL_PORT_LIST based on their ID
@@ -252,15 +234,17 @@ if __name__ == "__main__":
                 f"{bcolors.FAIL}No available port, exiting{bcolors.ENDC}",
                 file=sys.stderr,
             )
-            data_connection.send(b"close")
-            data_connection.close()
+            if data_connection:
+                data_connection.send(b"close")
+                data_connection.close()
             sys.exit(1)
 
         for serial_controller in serial_controller_list:
             print(
                 f"{bcolors.OKCYAN}Writing to port {serial_controller.port} : DEVICE {serial_controller.device_id}{bcolors.ENDC}"
             )
-            data_connection.send(serial_controller.device_id.encode())
+            if data_connection:
+                data_connection.send(serial_controller.device_id.encode())
             try:
                 serial_controller.write(serial_controller.beginFlag)
                 print(f"{bcolors.OKGREEN}Start flag sent{bcolors.ENDC}")
@@ -296,11 +280,13 @@ if __name__ == "__main__":
             print(
                 f"{bcolors.WARNING}Port {serial_controller.port} closed{bcolors.ENDC}"
             )
-        data_connection.send(b"close")
-        data_connection.close()
+        if data_connection:
+            data_connection.send(b"close")
+            data_connection.close()
         sys.exit(0)
 
     print(f"{bcolors.OKGREEN}All ports closed{bcolors.ENDC}")
-    data_connection.send(b"close")
-    data_connection.close()
+    if data_connection:
+        data_connection.send(b"close")
+        data_connection.close()
     sys.exit(0)
